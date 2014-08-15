11c961fb9e3c618459053e2203c4a1654b9b1e1a
wangyb <marcuswangyb@gmail.com>
2014-08-15 19:47:09 +0800

    balloon hit init

---
+++ Mushroom/src/resource.js
@@ -0,0 +1,20 @@
+var s_forest1 = "background.jpg";
+var s_mushroom = "mushroom.png";
+var s_mushroom2 = "mushroom2.png";
+
+var g_resources = [
+    //image
+    s_forest1,
+    s_mushroom,
+    s_mushroom2
+
+    //plist
+
+    //fnt
+
+    //tmx
+
+    //bgm
+
+    //effect
+];
